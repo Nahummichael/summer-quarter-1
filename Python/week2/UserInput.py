b7ec9ec6c3696b6c37ef9f6f65785836ee660d39
Nahum Michael <nahum.michaelhgs@.hiddengeniusproject.org>
2025-06-25 13:49:14 -0700

    UserInput ET

---
+++ Python/week2/UserInput.py
@@ -0,0 +1,10 @@
+name = input('who is you:')
+print('hello,'+ name + ", cool to meet you")
+
+
+grade = input('what grade are you:')
+print('oh so your in' + grade + ',cool')
+
+
+age = input('how old are you:')
+print ('so your,' + age + ', yrs you old')
