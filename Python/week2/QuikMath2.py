094feb7d020d92a8c749d450b8234764acd92430
Nahum Michael <nahum.michaelhgs@.hiddengeniusproject.org>
2025-06-26 14:06:17 -0700

    QuikMath2 ET

---
+++ Python/week2/QuikMath2.py
@@ -0,0 +1,23 @@
+def Add(addend1, addend2):
+    return addend1 + addend2
+
+sum = Add(5, 10)
+print(sum)
+
+def Multiply(factor1, factor2):
+    return factor1 * factor2
+
+product = Multiply(5, 2)
+print(product)
+
+def Subtract(subtractor1, subtractor2):
+    return subtractor1 - subtractor2
+
+subtracted = Subtract(10,5)
+print(subtracted)
+
+def divide(dividened1, dividened2):
+    return dividened1 / dividened2
+
+divided = divide(100,25)
+print(divided)
